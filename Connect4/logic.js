36e3f46cd8f7196b98bbea488e2cda50145c7e06
Your Name <you@example.com>
2026-07-14 21:54:20 +0500

    connect4 logic and script

--- Connect4/logic.js
+++ Connect4/logic.js
@@ -6,63 +6,35 @@ const table = [
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""],
 ];
-function updateTable(row, col, value) {
-    if (table[row][col] !== "") {
-        console.log("Cell Already Occupied");
-        return false;
+
+function updateTable(col, value) {
+    for (let i = 5; i > -1; i--) {
+        if (table[i][col] === "") {
+            table[i][col] = value;
+            return true;
+        }
     }
-    table[row][col] = [value];
-    return true;
+    return false;
 };
-function getCell(m, n) {
-    return table[m][n];
-};
-function compareCells(m1, n1, m2, n2) {
-    const A = getCell(m1, n1);
-    const B = getCell(m2, n2);
-    if (A !== "" && B !== "" && A === B) {
-        return true;
-    }
-};
+
+
 function winner() {
-    //Rows
-    let row = 0;
-    while (row < 6) {
-        if (compareCells(row, 0, row, 1) && compareCells(row, 0, row, 2) && compareCells(row, 0, row, 3)) {
+    //for rows
+    for (let i = 0; i < 6; i++) {
+        let row = table[i].join("");
+        let win = row.match(/gggg/);
+        if (win) {
             return true;
-        } else
-            if (compareCells(row, 1, row, 2) && compareCells(row, 1, row, 3) && compareCells(row, 1, row, 4)) {
-                return true;
-            } else
-                if (compareCells(row, 2, row, 3) && compareCells(row, 2, row, 4) && compareCells(row, 2, row, 5)) {
-                    return true;
-                } else
-                    if (compareCells(row, 3, row, 4) && compareCells(row, 3, row, 5) && compareCells(row, 3, row, 6)) {
-                        return true;
-                    }
-        row++;
+        }
     }
-    //Columns
-    let col = 0;
-    while (col < 7) {
-        if (compareCells(0, col, 1, col) && compareCells(0, col, 2, col) && compareCells(0, col, 3, col)) {
-            return true;
-        } else
-            if (compareCells(1, col, 2, col) && compareCells(1, col, 3, col) && compareCells(1, col, 4, col)) {
-                return true;
-            } else
-                if (compareCells(2, col, 3, col) && compareCells(2, col, 4, col) && compareCells(2, col, 5, col)) {
-                    return true;
-                }
-
-        col++;
-    }
-    //Primary Diagonls
-
-    //Secondary Diagonals
+    //for columns
+    //for diagonal
+    //for adiagonal
+    return false;
 };
 
 
 module.exports = {
-
+updateTable,
+table
 }
--- Connect4/script.js
+++ Connect4/script.js
@@ -1,10 +1,25 @@
 const readline = require('node:readline/promises');
 const { stdin, stdout } = require('node:process');
-const {  } = require('./logic');
+const { table, updateTable } = require('./logic');
 
 async function askQuestion(inputPrompt) {
-const rl = readline.createInterface({ input: stdin, output: stdout });
-const answer = await rl.question(inputPrompt);
+    const rl = readline.createInterface({ input: stdin, output: stdout });
+    const answer = await rl.question(inputPrompt);
     rl.close();
     return answer;
 }
+async function gameLoop() {
+    while (true) {
+        let A = await askQuestion(`Choose your column (1-7):`);
+        if (A < 1 || A > 7) {
+            console.log("Enter correct value of column");
+            console.log("Column must be from 1 to 7");
+            continue;
+        }
+        updateTable(A - 1, "g");
+        let B = Math.floor(Math.random() * 6);
+        updateTable(B, "r");
+        console.log(table);
+    }
+}
+gameLoop();
