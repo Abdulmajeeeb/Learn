1a1749d21b1beddca2c68dfc792c578db3aee32a
Your Name <you@example.com>
2026-07-16 19:22:19 +0500

    connect4 Terminal without comments

--- Connect4/logic.js
+++ Connect4/logic.js
@@ -8,7 +8,7 @@
 // ];
 let ROWS = 6;
 let COLS = 7;
-let wIN = 4;
+let WIN = 4;
 let table = [];
 for (let i = 0; i < ROWS; i++) {
     table.push([]);
@@ -29,12 +29,12 @@ function updateTable(col, value) {
 };
 
 
-function winner() {
+function gameEnd() {
     //for rows
     for (let i = 0; i < ROWS; i++) {
         let rowString = table[i].join("");
-        let win = rowString.match(/gggg/);
-        let lose = rowString.match(/rrrr/);
+        let win = rowString.match(/🟢🟢🟢🟢/);
+        let lose = rowString.match(/🔴🔴🔴🔴/);
         if (win) {
             return true;
         }
@@ -48,8 +48,8 @@ function winner() {
         for (let j = 0; j < ROWS; j++) {
             columnString += table[j][i]
         }
-        let win = columnString.match(/gggg/);
-        let lose = columnString.match(/rrrr/);
+        let win = columnString.match(/🟢🟢🟢🟢/);
+        let lose = columnString.match(/🔴🔴🔴🔴/);
         if (win) {
             return true;
         }
@@ -58,13 +58,55 @@ function winner() {
         }
     }
     //for diagonal
+    for (i = 0; i < ROWS - (WIN - 1); i++) {
+        for (j = 0; j < COLS - (WIN - 1); j++) {
+            let diagonalString = "";
+            for (k = 0; k < WIN; k++) {
+                diagonalString += table[i + k][j + k];
+            }
+            let win = diagonalString.match(/🟢🟢🟢🟢/);
+            let lose = diagonalString.match(/🔴🔴🔴🔴/);
+            if (win) {
+                return true;
+            }
+            if (lose) {
+                return true;
+            }
+        }
+    }
     //for adiagonal
+    for (i = 0; i < ROWS - (WIN - 1); i++) {
+        for (j = 3; j < COLS; j++) {
+            let aDiagonalString = "";
+            for (k = 0; k < WIN; k++) {
+                aDiagonalString += table[i + k][j - k];
+            }
+            let win = aDiagonalString.match(/🟢🟢🟢🟢/);
+            let lose = aDiagonalString.match(/🔴🔴🔴🔴/);
+            if (win) {
+                return true;
+            }
+            if (lose) {
+                return true;
+            }
+        }
+    }
     return false;
 };
 
+function resetTable(){
+    for(i=0;i<ROWS;i++){
+        for(j=0; j<COLS; j++){
+            table[i][j]=".";
+        }
+    }
+}
 
 module.exports = {
     updateTable,
     table,
-    winner
+    gameEnd,
+    ROWS,
+    COLS,
+    resetTable
 }
--- Connect4/script.js
+++ Connect4/script.js
@@ -1,6 +1,7 @@
 const readline = require('node:readline/promises');
 const { stdin, stdout } = require('node:process');
-const { table, updateTable, winner } = require('./logic');
+const { table, updateTable, gameEnd, ROWS, COLS, resetTable } = require('./logic');
+let moves = 0;
 
 async function askQuestion(inputPrompt) {
     const rl = readline.createInterface({ input: stdin, output: stdout });
@@ -8,29 +9,59 @@ async function askQuestion(inputPrompt) {
     rl.close();
     return answer;
 }
-async function gameLoop() {
+async function gameSystem() {
     while (true) {
         console.table(table);
         let A = Number(await askQuestion(`Choose your column (0-7):`));
-        if (A < 0 || A > 6) {
+        if (A < 0 || A > COLS - 1 || isNaN (A)) {
             console.log("Enter correct value of column");
             console.log("Column must be from 1 to 7");
             continue;
         }
-        updateTable(A, "g");
-        if(winner()) {
+        let placed = updateTable(A, "🟢");
+        if (!placed) {
+            console.log("No space, choose another column")
+            continue;
+        }
+
+
+        if (gameEnd()) {
             console.table(table);
             console.log("Green Wins");
             break;
         }
-        let B = Math.floor(Math.random() * 7);
-        updateTable(B, "r");
-        if(winner()) {
+        moves++;
+        if (moves === ROWS * COLS) {
+            console.log("DRAW");
+        }
+        let B;
+        do { B = Math.floor(Math.random() * COLS); }
+        while (!updateTable(B, "🔴"));
+        if (gameEnd()) {
             console.table(table);
             console.log("Red Wins");
+            break;
+        }
+        moves++;
+        if (moves === ROWS * COLS) {
+            console.log("DRAW");
             break;
         }
 
     }
 }
-gameLoop();
+async function main() {
+    while (true) {
+        moves = 0;
+        resetTable();
+        await gameSystem();
+        let again = await askQuestion("Do you want to play again? (y/n)");
+        if (again === "y" || again === "Y") {
+            continue;
+        } else {
+            break;
+        }
+    }
+}
+main();
+
