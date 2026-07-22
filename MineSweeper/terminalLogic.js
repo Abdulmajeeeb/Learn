c820c70cd8ddbdd389f92003068e7b9cee215951
Your Name <you@example.com>
2026-07-22 19:26:41 +0500

    MineSweeper, Terminal almost done

--- MineSweeper/terminalLogic.js
+++ MineSweeper/terminalLogic.js
@@ -1,26 +1,69 @@
-let rows=9;
-let cols=9;
-let board=[];
-let percentBomb=20;
-
-for (let i=0; i<rows; i++){
-    board.push([]);
-    for (let j=0; j<cols; j++){
-        board[i].push("");
-    }
+let board = [];
+let percentBomb = 20;
+function makeBoard(rows, cols) {
+    for (let i = 0; i < rows; i++) {
+        board.push([]);
+        for (let j = 0; j < cols; j++) {
+            board[i].push(0);
+        }
+    };
+    return;
 };
 
-console.table(board);
-let numberBomb= Number(Math.floor((percentBomb*rows*cols)/100));
-for (let i=0; i<numberBomb; i++){
-    let compRow=Number(Math.floor(Math.random()*rows));
-    let compCol=Number(Math.floor(Math.random()*cols));
-    if (board[compRow][compCol]!==""){
-        i--;
-    continue;
-    } else{
-        board[compRow][compCol]="x";
+function placeBombs(rows, cols) {
+    let numberBomb = Number(Math.floor((percentBomb * rows * cols) / 100));
+    for (let i = 0; i < numberBomb; i++) {
+        let compRow = Number(Math.floor(Math.random() * rows));
+        let compCol = Number(Math.floor(Math.random() * cols));
+        if (board[compRow][compCol] !== 0) {
+            i--;
+            continue;
+        } else {
+            board[compRow][compCol] = "x";
+        }
     }
+    // console.table(board);
+    // console.log(numberBomb);
+};
+
+//Place neighbours
+function neighbours(rows, cols) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (board[i][j] === "x") {
+                for (let p = -1; p < 2; p++) {
+                    let newRow = i + p;
+                    for (let q = -1; q < 2; q++) {
+                        let newCol = j + q;
+                        if (p === 0 && q === 0) {
+                            continue;
+                        }
+                        if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) {
+                            continue;
+                        }
+                        //update the neighbours with number of bombs in the surrounding
+                        if (board[newRow][newCol] !== "x") {
+                            board[newRow][newCol]++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // console.table(board);
 }
-console.table(board);
-console.log(numberBomb);
+
+
+// function gameEnd(){
+// if (displayTable(m,n)===bomb){
+//     return lose;
+// }
+// }
+
+module.exports = {
+    board,
+    placeBombs,
+    makeBoard,
+    neighbours,
+    // gameEnd
+}
--- MineSweeper/terminalScript.js
+++ MineSweeper/terminalScript.js
@@ -1,18 +1,62 @@
 const readline = require('node:readline/promises');
 const { stdin, stdout } = require('node:process');
-
-// Calling API
-const {board} = require('./terminalLogic');
 const console = require('node:console');
 
+// Calling API
+const { board, placeBombs, makeBoard, neighbours } = require('./terminalLogic');
+
+
 async function askQuestion(inputPrompt) {
-    const rl = readline.createInterface({ input:stdin, output:stdout });
+    const rl = readline.createInterface({ input: stdin, output: stdout });
     const answer = await rl.question(inputPrompt);
     rl.close();
     return answer;
 }
 
+function displayTable(rows, cols) {
+
+    // console.table(table);  
+};
 
 
 
+async function gameLoop() {
+    const rows = Number(await askQuestion("Enter number of Rows for you table: "));
+    const cols = Number(await askQuestion("Enter number of Columns for your table: "));
+    //make display table
+    let table = [];
+    for (let i = 0; i < rows; i++) {
+        table.push([]);
+        for (let j = 0; j < cols; j++) {
+            table[i].push("");
+        }
+    }
+    makeBoard(rows, cols);
+    // console.table(board);
+    placeBombs(rows, cols);
+    // console.table(board);
+    neighbours(rows, cols);
+    console.table(board)
+    while (true) {
+        console.table(table);
+        const getRow = Number(await askQuestion("Enter Cell Row Index: "));
+        if (getRow < 0 || getRow >= rows || getRow==isNaN()) {
+            console.log("Enter correct ROW INDEX");
+            continue;
+        }
+        const getCol = Number(await askQuestion("Enter Cell Cell Index: "));
+                if (getCol < 0 || getCol >= cols|| getRow==isNaN()) {
+            console.log("Enter correct COLUMN INDEX");
+            continue;
+        }
+        //Update Table
+        table[getRow][getCol] = board[getRow][getCol];
+        if (table[getRow][getCol] === "x") {
+            console.table(table);
+            console.log("YOU LOSE");
+            return false;
+        }
+    }
+};
+gameLoop();
 
