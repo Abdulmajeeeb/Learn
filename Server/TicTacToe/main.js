3180bf020cfbfd4af4a6218c5200e799b26d9aa1
Your Name <you@example.com>
2026-07-29 17:31:55 +0500

    Serve index.html from Express

--- Server/TicTacToe/main.js
+++ Server/TicTacToe/main.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const PORT = 3000;
 
 app.get("/",function(request, response){
-    response.send("Hello World");
+    response.sendFile(path.join(__dirname,'public','index.html'));
 });
 app.listen(PORT, function(){
     console.log(`Server is running on port ${PORT}`);
