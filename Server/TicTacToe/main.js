8a1bd4dcc804db171fdae0ad5a376ea42a37ea24
Your Name <you@example.com>
2026-07-30 19:46:58 +0500

    Implement manual routing and serve static files with Node.js

--- Server/TicTacToe/main.js
+++ Server/TicTacToe/main.js
@@ -2,15 +2,20 @@ const http = require('node:http');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const filePath = path.join(__dirname, "public", "index.html");
+const index = path.join(__dirname, "public", "index.html");
+const script = path.join(__dirname, "public", "script.js");
 
 const server = http.createServer(function (request, response) {
     const url = request.url;
     console.log(request.url);
     switch (url) {
         case "/":
-
-            response.end(fs.readFileSync(filePath));
+            response.writeHead(200,{'Content-Type':'text/html'});
+            response.end(fs.readFileSync(index));
+            break;
+        case "/script.js":
+            response.writeHead(200,{'Content-Type':'text/javascript'});
+            response.end(fs.readFileSync(script));
             break;
         case "/about":
             response.end("About page");
